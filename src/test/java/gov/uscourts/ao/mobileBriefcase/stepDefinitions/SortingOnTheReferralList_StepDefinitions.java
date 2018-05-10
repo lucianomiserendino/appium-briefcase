27200c59132c0d722e33f065e2933b1e77075fec
Saltanat Kasabolotova <>
2018-05-10 15:02:59 -0400

    SortingOnTheReferralListPage

--- src/test/java/gov/uscourts/ao/mobileBriefcase/stepDefinitions/SortingOnTheReferralList_StepDefinitions.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/stepDefinitions/SortingOnTheReferralList_StepDefinitions.java
@@ -1,10 +1,11 @@
 package gov.uscourts.ao.mobileBriefcase.stepDefinitions;
 
+import static java.util.Collections.reverse;
+import static java.util.Collections.sort;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -21,42 +22,58 @@ public class SortingOnTheReferralList_StepDefinitions {
 
 	}
 
-	@When("^User selects the sort button\\. User verifies the Date Down Arrow is selected by default and verifies the referrals are sorted by referred date in descending order \\(newest first\\)\\.$")
-	public void user_selects_the_sort_button_User_verifies_the_Date_Down_Arrow_is_selected_by_default_and_verifies_the_referrals_are_sorted_by_referred_date_in_descending_order_newest_first() {
-		try {
-			List<String> defaulOrder = page.referralsInDefaultOrder();
-			page.selectSortBtn();
-			List<String> descOrder = page.referralsInDescendingOrder();
+	@When("^User selects the sort button, clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in descending order \\(newest first\\)\\.$")
+	public void user_selects_the_sort_button_clicks_the_Date_Up_Arrow_button_and_verifies_the_referrals_are_sorted_by_referred_date_in_descending_order_newest_first() {
 
-			assertTrue("DROP DOWN ARROW OR REFERRALS BY DEFAULT ARE NOT SORTED IN DESCENDING ORDER",
-					defaulOrder.equals(descOrder));
-		} catch (AssertionError ex) {
-			ex.printStackTrace();
-		}
+		page.selectSortBtn();
+
+		List<String> referralsSotedByDatesInDescendingOrder = page.referralsSortedByDatesInDescendingOrder();
+		sort(referralsSotedByDatesInDescendingOrder);
+		reverse(referralsSotedByDatesInDescendingOrder);
+
+		List<String> referralsSotedByDateInAscendingOrder = page.referralsSortedByDateInAscendingOrder();
+		reverse(referralsSotedByDateInAscendingOrder);
+		assertTrue("REFERRALS ARE NOT SORTED BY REFERRED DATE IN DESCENDING ORDER",
+				referralsSotedByDatesInDescendingOrder.equals(referralsSotedByDateInAscendingOrder));
+
 	}
 
-	@And("^User clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in ascending order \\(oldest first\\)\\.$")
+	@When("^User clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in ascending order \\(oldest first\\)\\.$")
 	public void user_clicks_the_Date_Up_Arrow_button_and_verifies_the_referrals_are_sorted_by_referred_date_in_ascending_order_oldest_first() {
-		try {
-			List<String> descOrder = page.referralsInDescendingOrder();
-			List<String> ascOrder = page.verifyReferralsInAscendingOrder();
 
-			assertTrue("DATE UP ARROW BUTTON IS NOT SORTING IN ASCENDING ORDER", descOrder.equals(ascOrder));
-		} catch (AssertionError ex) {
-			ex.printStackTrace();
-		}
+		List<String> referralsSotedByDatesInDescendingOrder = page.referralsSortedByDatesInDescendingOrder();
+		sort(referralsSotedByDatesInDescendingOrder);
+
+		List<String> referralsSotedByDateInAscendingOrder = page.referralsSortedByDateInAscendingOrder();
+		assertTrue("REFERRALS ARE NOT SORTED BY REFERRED DATE IN ASCENDING ORDER",
+				referralsSotedByDatesInDescendingOrder.equals(referralsSotedByDateInAscendingOrder));
+
 	}
 
-	@Then("^User clicks the Case Down Arrow button and  verifies the referrals are sorted by case number in descending order after User clicks the Case Up Arrow button, verifies the referrals are sorted by case number in ascending order\\.$")
-	public void user_clicks_the_Case_Down_Arrow_button_and_verifies_the_referrals_are_sorted_by_case_number_in_descending_order_after_User_clicks_the_Case_Up_Arrow_button_verifies_the_referrals_are_sorted_by_case_number_in_ascending_order() {
-		try {
-			List<String> caseDescOrder = page.referralsSortedByCaseNumInDescOrder();
-			List<String> caseAscOrder = page.referralsSortedByCaseNumInAscOrder();
+	@Then("^User clicks the Case Down Arrow button and  verifies the referrals are sorted by case number in descending order$")
+	public void user_clicks_the_Case_Down_Arrow_button_and_verifies_the_referrals_are_sorted_by_case_number_in_descending_order() {
+		List<String> referralsSortedByCasesInDescendingOrder = page.referralsSortedByCasesInDescendingOrder();
+		sort(referralsSortedByCasesInDescendingOrder);
+		reverse(referralsSortedByCasesInDescendingOrder);
 
-			assertTrue("REFERRALS ARE NOT SORTED BY CASE NUMBER", caseDescOrder.equals(caseAscOrder));
-		} catch (AssertionError ex) {
-			ex.printStackTrace();
-		}
+		List<String> referralsSortedByCasesInAscendingOrder = page.referralsSortedByCasesInAscendingOrder();
+		reverse(referralsSortedByCasesInAscendingOrder);
+
+		assertTrue("REFERRALS  ARE NOT SORTED BY CASE NUMBER IN DESCENDING ORDER",
+				referralsSortedByCasesInDescendingOrder.equals(referralsSortedByCasesInAscendingOrder));
+
 	}
 
+	@Then("^User clicks the Case Up Arrow button, verifies the referrals are sorted by case number in ascending order\\.$")
+	public void user_clicks_the_Case_Up_Arrow_button_verifies_the_referrals_are_sorted_by_case_number_in_ascending_order()
+			throws Throwable {
+		List<String> referralsSortedByCasesInDescendingOrder = page.referralsSortedByCasesInDescendingOrder();
+		sort(referralsSortedByCasesInDescendingOrder);
+
+		List<String> referralsSortedByCasesInAscendingOrder = page.referralsSortedByCasesInAscendingOrder();
+
+		assertTrue("REFERRALS  ARE NOT SORTED BY CASE NUMBER IN ASCENDING ORDER",
+				referralsSortedByCasesInDescendingOrder.equals(referralsSortedByCasesInAscendingOrder));
+
+	}
 }
