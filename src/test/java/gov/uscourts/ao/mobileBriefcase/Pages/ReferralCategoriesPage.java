7028c8097431423bd04de305489f53406248eab5
Saltanat Kasabolotova <>
2018-06-12 08:07:56 -0400

    Referral categories for the judge

--- src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/ReferralCategoriesPage.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/ReferralCategoriesPage.java
@@ -1,16 +1,27 @@
 package gov.uscourts.ao.mobileBriefcase.Pages;
 
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.executeQuery;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.MOTIONS_PETITIONS_SI_VALUE_N;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.MOTIONS_PETITIONS_SI_VALUE_Y;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.PETITIONS_FOR_REHEARING_SI_VALUE_N;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.PETITIONS_FOR_REHEARING_SI_VALUE_Y;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.SCREENING_PANELS_SI_VALUE_N;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.SCREENING_PANELS_SI_VALUE_Y;
 import static gov.uscourts.ao.mobileBriefcase.common.Base.driver;
+import static gov.uscourts.ao.mobileBriefcase.common.BriefcaseUsers.select;
 import static gov.uscourts.ao.mobileBriefcase.common.Page.performPageLoad;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.clickOn;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.getListOfCategories;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.getNumOfDisplayedCases;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.support.PageFactory;
 
+import gov.uscourts.ao.mobileBriefcase.common.BriefcaseUsers.Users;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.pagefactory.WithTimeout;
@@ -23,18 +34,13 @@ public class ReferralCategoriesPage {
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
 
-	
 	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
 	@iOSFindBy(accessibility = "Pending Tasks")
 	public static MobileElement pendingTasks;
 
-
-
+	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
 	@iOSFindBy(xpath = "//*[contains(@name, 'Total')]")
-	public static MobileElement numberOfNonOrallyARGCases;
-
-	@iOSFindBy(xpath = "//*[contains(@name, 'Total')]")
-	public static MobileElement totalNumOfPendingTasks;
+	public static MobileElement total;
 
 	@iOSFindBy(accessibility = "Screening Panels")
 	public static MobileElement screeningPanels;
@@ -54,7 +60,7 @@ public class ReferralCategoriesPage {
 			clickOn(pendingTasks);
 		}
 		performPageLoad();
-		return getNumOfDisplayedCases(totalNumOfPendingTasks, "start", "peningTskEnd");
+		return getNumOfDisplayedCases(total);
 
 	}
 
@@ -63,10 +69,42 @@ public class ReferralCategoriesPage {
 
 	}
 
-	public String getNumOfdisplayedCases() {
-		clickOn(motionsPetitions);
+	public void verifyNonOrallyArgCases() {
+		getNonOrallyARGCases(PETITIONS_FOR_REHEARING_SI_VALUE_N, PETITIONS_FOR_REHEARING_SI_VALUE_Y,
+				verifyNumOfNonOrallyARGCases(petitionsForRehearing));
+		select(Users.DASHBOARD);
 		performPageLoad();
-		return getNumOfDisplayedCases(numberOfNonOrallyARGCases, "start", "end");
+		getNonOrallyARGCases(MOTIONS_PETITIONS_SI_VALUE_N, MOTIONS_PETITIONS_SI_VALUE_Y,
+				verifyNumOfNonOrallyARGCases(motionsPetitions));
+		select(Users.DASHBOARD);
+		performPageLoad();
+		getNonOrallyARGCases(SCREENING_PANELS_SI_VALUE_N, SCREENING_PANELS_SI_VALUE_Y,
+				verifyNumOfNonOrallyARGCases(screeningPanels));
+
+	}
+
+	public void getNonOrallyARGCases(String SI_VALUE_N, String SI_VALUE_Y, String uiNonOrallyARGCases) {
+
+		List<String> DBnonOrallyarguedCases_SI_VALUE_N = executeQuery(SI_VALUE_N);
+		List<String> DBnonOrallyarguedCases_SI_VALUE_Y = executeQuery(SI_VALUE_Y);
+
+		List<String> UInonOrallyarguedCases = Arrays.asList(uiNonOrallyARGCases);
+
+		assertTrue("-----RECORD COUNT MISMATCHED-----",
+				DBnonOrallyarguedCases_SI_VALUE_N.containsAll(UInonOrallyarguedCases)
+						|| DBnonOrallyarguedCases_SI_VALUE_Y.containsAll(UInonOrallyarguedCases));
+
+	}
+
+	public String verifyNumOfNonOrallyARGCases(MobileElement element) {
+		return uiNonOrallyARGCases(element);
+
+	}
+
+	public String uiNonOrallyARGCases(MobileElement element) {
+		clickOn(element);
+		performPageLoad();
+		return getNumOfDisplayedCases(total);
 	}
 
 }
