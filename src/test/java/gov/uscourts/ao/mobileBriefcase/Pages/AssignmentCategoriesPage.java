e2fef9fd39c0f9a48d895b2573ed6d96e8f332fb
Saltanat Kasabolotova <>
2018-06-12 08:03:40 -0400

    Obtain assignment categories

--- src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/AssignmentCategoriesPage.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/AssignmentCategoriesPage.java
@@ -1,20 +1,24 @@
 package gov.uscourts.ao.mobileBriefcase.Pages;
 
 import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.executeQuery;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.ASSIGNMENT_CATEGORIES;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.REFERRAL_CATEGORIES;
 import static gov.uscourts.ao.mobileBriefcase.common.Base.driver;
-import static gov.uscourts.ao.mobileBriefcase.common.Helper.getCategories;
+import static gov.uscourts.ao.mobileBriefcase.common.BriefcaseUsers.Users.STAFF_ATTORNEYS;
 import static gov.uscourts.ao.mobileBriefcase.common.Helper.getReferralCategories;
 import static gov.uscourts.ao.mobileBriefcase.common.Page.performPageLoad;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.getCollapsablePanel;
+import static gov.uscourts.ao.mobileBriefcase.common.Utilities.getText;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.selectAUser;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.support.PageFactory;
 
-import gov.uscourts.ao.mobileBriefcase.DBUtils.Queries;
 import gov.uscourts.ao.mobileBriefcase.common.BriefcaseUsers.Users;
 import gov.uscourts.ao.mobileBriefcase.common.Constants;
 import io.appium.java_client.MobileElement;
@@ -22,29 +26,31 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.pagefactory.WithTimeout;
 import io.appium.java_client.pagefactory.iOSFindBy;
 
-
 public class AssignmentCategoriesPage implements Constants {
-
 
 	public AssignmentCategoriesPage() {
 
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
 
-	private int startRow = 1;
-	private int endRow = 3;
-	private int cellIndex = 2;
-	private String name = "Categories";
 
-
-
-	
 	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
 	@iOSFindBy(xpath = "//*[contains(@name, 'User')]")
 	public static MobileElement selectUser;
 
-	
-	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
+	@WithTimeout(time = 15, unit = TimeUnit.SECONDS)
+	@iOSFindBy(id = "My Cases")
+	public static MobileElement my_Cases;
+
+	@WithTimeout(time = 15, unit = TimeUnit.SECONDS)
+	@iOSFindBy(xpath = "//XCUIElementTypeTable[@name='Categories']/XCUIElementTypeCell[3]/XCUIElementTypeStaticText[2]")
+	public static MobileElement senior_Staff_Attorney;
+
+	@WithTimeout(time = 15, unit = TimeUnit.SECONDS)
+	@iOSFindBy(id = "Unassigned")
+	public static MobileElement unassigned;
+
+	@WithTimeout(time = 15, unit = TimeUnit.SECONDS)
 	@iOSFindBy(xpath = "//*[contains(@name, 'Senior Staff Attorney')]")
 	public static MobileElement staffAttorney;
 
@@ -52,13 +58,12 @@ public class AssignmentCategoriesPage implements Constants {
 
 		selectAUser(Users.BROWN_BENJAMIN);
 
-
 	}
 
 	public void getAssignmentCategories() {
-		List<String> uiAssignmentCategories = getCategories(name, startRow, endRow, cellIndex);
-
-		List<String> dbAssignmentCategories = executeQuery(Queries.ASSIGNMENT_CATEGORIES);
+		List<String> uiAssignmentCategories = getListOfCategories();
+		List<String> dbAssignmentCategories = executeQuery(ASSIGNMENT_CATEGORIES);
+	
 		assertDbContainsAllFromUi(dbAssignmentCategories, uiAssignmentCategories);
 	}
 
@@ -71,8 +76,7 @@ public class AssignmentCategoriesPage implements Constants {
 	public void osberveReferralCategories() {
 
 		List<String> uiRefCategories = getReferralCategories();
-		List<String> dbuiRefCategories = executeQuery(Queries.REFERRAL_CATEGORIES);
-
+		List<String> dbuiRefCategories = executeQuery(REFERRAL_CATEGORIES);
 		assertDbContainsAllFromUi(dbuiRefCategories, uiRefCategories);
 	}
 
@@ -88,11 +92,19 @@ public class AssignmentCategoriesPage implements Constants {
 
 		finally {
 
-			getCollapsablePanel(selectUser, Users.STAFF_ATTORNEYS);
-
-			
+			getCollapsablePanel(selectUser, STAFF_ATTORNEYS);
 
 		}
+
+	}
+
+	public static List<String> getListOfCategories() {
+		List<String> categories = new ArrayList<>();
+		categories.add(getText(my_Cases));
+		categories.add(getText(senior_Staff_Attorney));
+		categories.add(getText(unassigned));
+		Collections.sort(categories);
+		return categories;
 
 	}
 
