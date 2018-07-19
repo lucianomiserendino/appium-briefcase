0581d3d7c12cce27e64309072376c2f3f2f313ac
Saltanat Kasabolotova <>
2018-07-19 09:16:31 -0400

    changing udid

--- src/test/java/gov/uscourts/ao/mobileBriefcase/common/Base.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/common/Base.java
@@ -41,8 +41,8 @@ public abstract class Base implements iOSCapabilities {
 				SetCapabilitiy(AUTO_ACCEPT_ALERTS);
 				SetCapabilitiy(TAKES_SCREENSHOT);
 
-				driver = new IOSDriver<MobileElement>(new URL(Configuration.getProperty("host")), capabilities);
-				//driver = new IOSDriver<MobileElement>(new URL(System.getProperty("remotewebdriver.url")), capabilities);
+				//driver = new IOSDriver<MobileElement>(new URL(Configuration.getProperty("host")), capabilities);
+				driver = new IOSDriver<MobileElement>(new URL(System.getProperty("remotewebdriver.url")), capabilities);
 
 				break;
 
@@ -70,8 +70,8 @@ public abstract class Base implements iOSCapabilities {
 			SetCapabilitiy(AUTO_ACCEPT_ALERTS);
 			SetCapabilitiy(ENSURING_CLEAN_SESSION);
 
-			driver = new IOSDriver<MobileElement>(new URL(Configuration.getProperty("host")), capabilities);
-			//driver = new IOSDriver<MobileElement>(new URL(System.getProperty("remotewebdriver.url")), capabilities);
+			//driver = new IOSDriver<MobileElement>(new URL(Configuration.getProperty("host")), capabilities);
+			driver = new IOSDriver<MobileElement>(new URL(System.getProperty("remotewebdriver.url")), capabilities);
 
 		} catch (MalformedURLException e) {
 
