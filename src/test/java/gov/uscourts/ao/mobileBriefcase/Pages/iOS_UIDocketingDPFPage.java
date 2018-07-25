b61574d1b339f587da058cb28fa39a06658ac814
Saltanat Kasabolotova <>
2018-07-25 12:18:06 -0400

    dpf dokcketing (cm3a)

--- src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/iOS_UIDocketingDPFPage.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/iOS_UIDocketingDPFPage.java
@@ -1,12 +1,13 @@
 package gov.uscourts.ao.mobileBriefcase.Pages;
 
 import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.getAllColumns;
-import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.getEL_Function;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.getID;
 import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.ACTION_NAME;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.MBR_NOTE;
 import static gov.uscourts.ao.mobileBriefcase.common.Helper.getPanel;
 import static gov.uscourts.ao.mobileBriefcase.common.Helper.Actions.ACTIONS;
-import static gov.uscourts.ao.mobileBriefcase.common.Helper.Actions.MOTIONS_PETITIONS;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.click;
+import static gov.uscourts.ao.mobileBriefcase.common.Utilities.getRestrictParam;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.isDisplayed;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.refresh;
 import static gov.uscourts.ao.mobileBriefcase.common.Utilities.selectCaseNumber;
@@ -21,7 +22,6 @@ import org.openqa.selenium.support.PageFactory;
 
 import gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.DBType;
 import gov.uscourts.ao.mobileBriefcase.common.Base;
-import static gov.uscourts.ao.mobileBriefcase.common.Constants.*;
 import gov.uscourts.ao.mobileBriefcase.common.Helper.Actions;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
@@ -33,9 +33,6 @@ public class iOS_UIDocketingDPFPage extends Base {
 	public iOS_UIDocketingDPFPage() {
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
-
-	static String actionName = "//XCUIElementTypeTable[@name='DocumentList']/child::*//*[contains(@name, '"
-			+ getAllColumns(DBType.CMKA,ACTION_NAME) + "')]";
 
 	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
 	@iOSFindBy(xpath = "//XCUIElementTypeTextView[1]")
@@ -66,43 +63,55 @@ public class iOS_UIDocketingDPFPage extends Base {
 	@iOSFindBy(xpath = "//XCUIElementTypeOther[4]/XCUIElementTypeOther/XCUIElementTypeStaticText")
 	public static MobileElement actionsName;
 
-	public void getCase(String caseNum) {
+	public void selectCase(String category, String caseNum) {
 		refresh();
-		selectCaseNumber(MOTIONS_PETITIONS, caseNum);
+		selectCaseNumber(category, caseNum);
 	}
 
-	public void getActionsPanel() {
-		clickOnPanel(ACTIONS, actionName);
+	public void getActionsPanel(DBType dbType, String el_id) {
+
+		getPanel(ACTIONS);
+		String action = "//XCUIElementTypeTable[@name='DocumentList']/child::*//*[contains(@name, '"
+				+ getAllColumns(dbType, getID(ACTION_NAME, el_id)) + "')]";
+
+		clickOnPanel(ACTIONS, action);
 
 	}
 
-	public void verifyActionName() {
-		verifyTextIsDisplayed(actionsName, getAllColumns(DBType.CMKA,ACTION_NAME).trim());
+	public void verifyActionName(DBType dbType, String el_id) {
+		verifyTextIsDisplayed(actionsName, getAllColumns(dbType, getID(ACTION_NAME, el_id)).trim());
 	}
 
 	public void verifyAddNewNoteDisplayed(String text) {
 		verifyTextIsDisplayed(addNewNote, text);
 	}
 
-	public void verifyFieldsAreDisplayed(String descriptionText, String commentText, String submitText) {
+	public void verifyFieldsAreDisplayed(String descriptionText, String commentText, String submitText, DBType dbType,
+			String el_id) {
 
 		verifyTextIsDisplayed(description, descriptionText);
-		getDefaulDescription();
+		getDefaulDescription(dbType, el_id);
 		verifyTextIsDisplayed(comments, commentText);
 		verifyTextIsDisplayed(submit, submitText);
 
 	}
 
-	public void getDefaulDescription() {
+	public void getDefaulDescription(DBType dbType, String el_id) {
 		try {
-			if (getEL_Function(COURT_USERS, 31).equals("SKIP")) {
+			if (getEL_Function(dbType, el_id, 31).equals("SKIP")) {
 				assertTrue(descriptionField.getText().equals("Transaction Note"));
 			} else {
-				assertTrue(getEL_Function(COURT_USERS, 31).equals(descriptionField.getText()));
+				assertTrue(getEL_Function(dbType, el_id, 31).equals(descriptionField.getText()));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static String getEL_Function(DBType dbType, String el_id, int index) {
+		final String mbrNote = getID(MBR_NOTE, el_id);
+		return getRestrictParam(getAllColumns(dbType, mbrNote), index);
+
 	}
 
 	public static void clickOnPanel(Actions panel, String penlRow) {
