db54e796a911011def9097b1867a9b58941c3695
Saltanat Kasabolotova <>
2018-07-30 13:30:36 -0400

    testing sort functinality on >> (cm3a)

--- src/test/java/gov/uscourts/ao/mobileBriefcase/stepDefinitions/SortingOnTheReferralList_StepDefinitions.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/stepDefinitions/SortingOnTheReferralList_StepDefinitions.java
@@ -20,59 +20,32 @@ public class SortingOnTheReferralList_StepDefinitions {
 		page.clickOnMotionsPetitions(category);
 	}
 
-	@When("^User selects the sort button, clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in descending order \\(newest first\\)\\.$")
-	public void user_selects_the_sort_button_clicks_the_Date_Up_Arrow_button_and_verifies_the_referrals_are_sorted_by_referred_date_in_descending_order_newest_first() {
+	@When("^User selects the sort button, clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in descending order \\(newest first\\)\\. User clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in ascending order \\(oldest first\\)\\.$")
+	public void user_selects_the_sort_button_clicks_the_Date_Up_Arrow_button_and_verifies_the_referrals_are_sorted_by_referred_date_in_descending_order_newest_first_User_clicks_the_Date_Up_Arrow_button_and_verifies_the_referrals_are_sorted_by_referred_date_in_ascending_order_oldest_first() {
 
 		page.selectSortBtn();
 
 		List<String> referralsSotedByDatesInDescendingOrder = page.referralsSortedByDatesInDescendingOrder();
 		sort(referralsSotedByDatesInDescendingOrder);
+
 		reverse(referralsSotedByDatesInDescendingOrder);
-		System.out.println(referralsSotedByDatesInDescendingOrder);
 
 		List<String> referralsSotedByDateInAscendingOrder = page.referralsSortedByDateInAscendingOrder();
 		reverse(referralsSotedByDateInAscendingOrder);
-		assertTrue("REFERRALS ARE NOT SORTED BY REFERRED DATE IN DESCENDING ORDER",
-				referralsSotedByDatesInDescendingOrder.equals(referralsSotedByDateInAscendingOrder));
 
+		assertTrue("REFERRALS ARE NOT SORTED BY  DATE",
+				referralsSotedByDatesInDescendingOrder.equals(referralsSotedByDateInAscendingOrder));
 	}
 
-	@When("^User clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in ascending order \\(oldest first\\)\\.$")
-	public void user_clicks_the_Date_Up_Arrow_button_and_verifies_the_referrals_are_sorted_by_referred_date_in_ascending_order_oldest_first() {
-
-		List<String> referralsSotedByDatesInDescendingOrder = page.referralsSortedByDatesInDescendingOrder();
-		sort(referralsSotedByDatesInDescendingOrder);
-
-		List<String> referralsSotedByDateInAscendingOrder = page.referralsSortedByDateInAscendingOrder();
-		assertTrue("REFERRALS ARE NOT SORTED BY REFERRED DATE IN ASCENDING ORDER",
-				referralsSotedByDatesInDescendingOrder.equals(referralsSotedByDateInAscendingOrder));
-
-	}
-
-	@Then("^User clicks the Case Down Arrow button and  verifies the referrals are sorted by case number in descending order$")
-	public void user_clicks_the_Case_Down_Arrow_button_and_verifies_the_referrals_are_sorted_by_case_number_in_descending_order() {
+	@Then("^User clicks the Case Down Arrow button and  verifies the referrals are sorted by case number in descending order \\. User clicks the Case Up Arrow button, verifies the referrals are sorted by case number in ascending order\\.$")
+	public void user_clicks_the_Case_Down_Arrow_button_and_verifies_the_referrals_are_sorted_by_case_number_in_descending_order_User_clicks_the_Case_Up_Arrow_button_verifies_the_referrals_are_sorted_by_case_number_in_ascending_order() {
 		List<String> referralsSortedByCasesInDescendingOrder = page.referralsSortedByCasesInDescendingOrder();
-		sort(referralsSortedByCasesInDescendingOrder);
-		reverse(referralsSortedByCasesInDescendingOrder);
 
 		List<String> referralsSortedByCasesInAscendingOrder = page.referralsSortedByCasesInAscendingOrder();
 		reverse(referralsSortedByCasesInAscendingOrder);
 
 		assertTrue("REFERRALS  ARE NOT SORTED BY CASE NUMBER IN DESCENDING ORDER",
 				referralsSortedByCasesInDescendingOrder.equals(referralsSortedByCasesInAscendingOrder));
-
 	}
 
-	@Then("^User clicks the Case Up Arrow button, verifies the referrals are sorted by case number in ascending order\\.$")
-	public void user_clicks_the_Case_Up_Arrow_button_verifies_the_referrals_are_sorted_by_case_number_in_ascending_order()
-			throws Throwable {
-		List<String> referralsSortedByCasesInDescendingOrder = page.referralsSortedByCasesInDescendingOrder();
-		sort(referralsSortedByCasesInDescendingOrder);
-
-		List<String> referralsSortedByCasesInAscendingOrder = page.referralsSortedByCasesInAscendingOrder();
-
-		assertTrue("REFERRALS  ARE NOT SORTED BY CASE NUMBER IN ASCENDING ORDER",
-				referralsSortedByCasesInDescendingOrder.equals(referralsSortedByCasesInAscendingOrder));
-
-	}
 }
--- src/test/resources/Features/CMKA&CM3A/SortingOnTheReferralListPage.feature
+++ src/test/resources/Features/CMKA&CM3A/SortingOnTheReferralListPage.feature
@@ -13,17 +13,14 @@ The default is by date referred in descending order (newest first).
 	And User clicks on Send Key to Device 
 	Then User navigates to MobileBrifcase App 
 	And  user selects a "<server>" 
-	When User selects Judge,  "<refCategory>"
-	When User selects the sort button, clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in descending order (newest first). 
-	And  User clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in ascending order (oldest first). 
-	Then User clicks the Case Down Arrow button and  verifies the referrals are sorted by case number in descending order 
-	Then  User clicks the Case Up Arrow button, verifies the referrals are sorted by case number in ascending order. 
+	When User selects Judge,  "<refCategory>" 
+	When User selects the sort button, clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in descending order (newest first). User clicks the Date Up Arrow button and verifies the referrals are sorted by referred date in ascending order (oldest first). 
+	Then User clicks the Case Down Arrow button and  verifies the referrals are sorted by case number in descending order . User clicks the Case Up Arrow button, verifies the referrals are sorted by case number in ascending order. 
 	
 	Examples: 
 		|environment   |userName          |password  |server                                   |refCategory       |
 		|Integration   |chambers courtney |Test2020!|Appellate DC Development - CMKA           |Motions/Petitions |
-		#|Testing       |Chambers Haenni   |Test2017!|Appellate DC Installation Testing - CM3A  |Motion/Petition   | 
+		|Testing       |Chambers Haenni   |Test2017!|Appellate DC Installation Testing - CM3A  |Motion/Petition   | 
 		
 		
-
 		
