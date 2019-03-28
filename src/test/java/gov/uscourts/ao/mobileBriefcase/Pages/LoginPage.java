ab18b17e24aea8f5fdfcf9f6ecb8d5d4ab040b9f
Saltanat Kasabolotova <>
2019-03-28 08:49:34 -0400

    changing accessibility to cmka

--- src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/LoginPage.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/LoginPage.java
@@ -66,7 +66,7 @@ public class LoginPage extends Base {
 	public WebElement sendKeyButton;
 
 	// @WithTimeout(time = 300, unit = TimeUnit.SECONDS)
-	@iOSFindBy(accessibility = "Appellate DC Development - CMKA JOPC")
+	@iOSFindBy(accessibility = "Appellate DC Development - CMKA")
 	public MobileElement cmka;
 
 	@iOSFindBy(accessibility = "Appellate DC Development - CM1A")
