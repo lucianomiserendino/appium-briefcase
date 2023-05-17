78d773e24538e1555c16e066e701e5bfe0b7055f
Saltanat Kasabolotova <saltanat_kasabolotova@ao.uscourts.gov>
2023-05-17 09:33:21 -0400

    AMB-3570 -- JudgeVote DPF - Same vote multiple times

--- src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/EnBancPage.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/EnBancPage.java
@@ -1,18 +1,37 @@
 package gov.uscourts.ao.mobileBriefcase.Pages;
 
 import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.executeQuery;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.getAllColumns;
 import static gov.uscourts.ao.mobileBriefcase.DBUtils.DBUtilities.getID;
+import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.JUDGE_VOTE_DPF_RELIEF;
 import static gov.uscourts.ao.mobileBriefcase.DBUtils.Queries.NON_ORALLY_ARGUED_CASES;
+import static gov.uscourts.ao.mobileBriefcase.Pages.CommonPages.getGroupIcons;
+import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.containsElement;
+import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.findElement;
+import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.getText;
+import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.isDisplayed;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.replace;
+import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.tap;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Page.performPageLoad;
+import static gov.uscourts.ao.mobileBriefcase.page.common.Utility.clickOnNumberInRange;
 import static java.util.Collections.sort;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import gov.uscourts.ao.mobileBriefcase.DBUtils.Queries;
+import gov.uscourts.ao.mobileBriefcase.Pages.CommonPages.GroupIcons;
+import gov.uscourts.ao.mobileBriefcase.Pages.CommonPages.Panel;
 import gov.uscourts.ao.mobileBriefcase.model.UserInputData;
+import gov.uscourts.ao.mobileBriefcase.page.common.Actions.Locator;
 import gov.uscourts.ao.mobileBriefcase.page.common.AppiumPageFactory;
+import gov.uscourts.ao.mobileBriefcase.page.common.Page;
 import gov.uscourts.ao.mobileBriefcase.page.common.Utility;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 
@@ -31,6 +50,14 @@ public class EnBancPage extends AppiumPageFactory {
 	public static List<WebElement> enBanc;
 
 	private static String xpath = "//XCUIElementTypeOther[@name='Categories']/XCUIElementTypeScrollView/XCUIElementTypeOther//XCUIElementTypeStaticText";
+
+	@iOSXCUITFindBy(xpath = "//XCUIElementTypeOther[@name='VoteOptions']/XCUIElementTypeScrollView/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeStaticText")
+	public static List<WebElement> judgeVotes;
+
+	@iOSXCUITFindBy(id = "Back")
+	public static WebElement back;
+
+	String select = "Please Select";
 
 	public void getViewVotes() {
 
@@ -73,4 +100,88 @@ public class EnBancPage extends AppiumPageFactory {
 		return caseNum;
 
 	}
+
+	public void selectRandomVote(List<UserInputData> userInputData, String caseNum) {
+		String ccr_id = CommonPages.getCCRID(caseNum, userInputData);
+
+		String voteText = "";
+		String text = "";
+		String reliefText = getRelief(ccr_id, userInputData);
+
+		tap(Locator.XPATH, getIndexOfVoteButton(reliefText, 1));
+
+		Page.sleep(4000);
+		List<WebElement> votes = judgeVotes;
+
+		String voteList = "";
+		Iterator<WebElement> i = votes.iterator();
+		while (i.hasNext()) {
+			WebElement row = i.next();
+			voteList += row.getText();
+		}
+
+		if (voteList.contains(select)) {
+			try {
+				votes.remove(select);
+
+			} catch (NoSuchElementException e) {
+				e.getMessage();
+			}
+
+			voteText += clickOnNumberInRange(votes);
+			text += ifNoVoteDisplayed(voteText, reliefText);
+
+		} else {
+			voteText += clickOnNumberInRange(votes);
+			text += ifNoVoteDisplayed(voteText, reliefText);
+		}
+
+		tap(Locator.XPATH, getIndexOfNoteIcon(reliefText));
+
+		tap(back);
+
+		CommonPages.getPanel(Panel.valueOf("Vote_Information"));
+		String relief = getRelief(ccr_id, userInputData);
+		findElement(By.xpath("(//XCUIElementTypeStaticText[@name='" + relief
+				+ "']/following::XCUIElementTypeOther[contains(@name, '" + text + "')])[1]")).click();
+		performPageLoad(driver);
+		assertTrue(isDisplayed(Locator.XPATH, containsElement("$$")));
+		String title = getAllColumns(Queries.DM_DESCRIPTION, userInputData);
+		assertTrue(isDisplayed(Locator.XPATH, containsElement(title)));
+
+	}
+
+	public static String getRelief(String ccr_id, List<UserInputData> userInputData) {
+		return getAllColumns(getID(JUDGE_VOTE_DPF_RELIEF, ccr_id), userInputData);
+
+	}
+
+	public static String getIndexOfVoteButton(String relief, int index) {
+		return "(//XCUIElementTypeStaticText[@name='" + relief
+				+ "']/following::XCUIElementTypeOther/XCUIElementTypeButton)[" + index + "]";
+
+	}
+
+	public static String getIndexOfNoteIcon(String relief) {
+		return "(//XCUIElementTypeStaticText[@name='" + relief
+				+ "']/following::XCUIElementTypeOther/XCUIElementTypeButton[@name='View Votes'][1]"
+				+ "/following::XCUIElementTypeOther//XCUIElementTypeStaticText)[1]";
+
+	}
+
+	public void verifyCourtAdminAccess() {
+		getGroupIcons(GroupIcons.Expand);
+		assertFalse(
+				"SITE TABLE VARIABLE \"BRIEFCASECTADMINDKT\" IS SET TO 'N', HOWEVER COURT ADMINS CAN SEE ACTIONS IN BRIEFCASE",
+				isDisplayed(Locator.XPATH, containsElement("Actions")));
+	}
+
+	public String ifNoVoteDisplayed(String voteText, String reliefText) {
+		if (voteText.equals("No Change")) {
+			return getText(Locator.XPATH, getIndexOfVoteButton(reliefText, 1));
+		} else {
+			return voteText;
+		}
+	}
+
 }
