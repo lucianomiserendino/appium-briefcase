7e57cc965990a1f0d61276143fe33d9c8fbe8912
Saltanat_Kasabolotova <saltanat_kasabolotova@ao.uscourts.gov>
2025-08-22 17:09:35 -0400

    AMB-5116 -- search function

--- src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/CaseQueryPage.java
+++ src/test/java/gov/uscourts/ao/mobileBriefcase/Pages/CaseQueryPage.java
@@ -3,26 +3,25 @@ package gov.uscourts.ao.mobileBriefcase.Pages;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.clicksOn;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.contains;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.containsElement;
+import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.findElementBy;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.isDisplayed;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Actions.sendKeys;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Page.performPageLoad;
-import static gov.uscourts.ao.mobileBriefcase.page.common.Page.waitForVisibilityOfElement;
 import static gov.uscourts.ao.mobileBriefcase.page.common.Utility.scrollDownIfNotDisplayed;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import gov.uscourts.ao.mobileBriefcase.page.common.Actions;
 import gov.uscourts.ao.mobileBriefcase.page.common.Actions.Locator;
 import gov.uscourts.ao.mobileBriefcase.page.common.AppiumPageFactory;
-import gov.uscourts.ao.mobileBriefcase.page.common.NetworkManager;
 import gov.uscourts.ao.mobileBriefcase.page.common.Page;
 import gov.uscourts.ao.mobileBriefcase.page.common.Utility;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
@@ -195,33 +194,6 @@ public class CaseQueryPage extends AppiumPageFactory {
 		return speChar;
 
 	}
-	
-public void searchFunctionIsDisabled() {
-	
-	  assertTrue( "Verify the magnifying glass icon is enabled",searchIcon.isEnabled());
-
-		// Switch to offline mode
-		try {
-		NetworkManager.makeScriptsExecutable();
-		NetworkManager.disableInternet();
-	} catch (IOException | RuntimeException e) {
-		System.err.println("Failed to disable internet: " + e.getMessage());
-	}
-
-	DashboardPage page = new DashboardPage();
-	
-	waitForVisibilityOfElement(page.offline_Indicator, driver);
-        
-    assertFalse("Verify the magnifying glass icon is disabled", searchIcon.isEnabled());
-
-	try {
-		NetworkManager.makeScriptsExecutable();
-		NetworkManager.enableInternet();
-	} catch (IOException | RuntimeException e) {
-		System.err.println("Failed to re-enable internet: " + e.getMessage());
-	
-}
-}
 
 	public enum Search {
 		wildcard, caseNumber, partyName
